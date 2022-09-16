1149d9f9e538daaceaed7b4197f2c63202d1309c
adrian-af <64954980+adrian-af@users.noreply.github.com>
2022-09-16 09:46:12 +0200

    Add files via upload

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function search(Request $request)
+    {
+        //$var = $request->search ."<br>";
+       /*  $busqueda = $_POST['busqueda'];
+        $order = $_POST['order'];
+        $desde = $_POST['desde'];
+        $hasta = $_POST['hasta']; */
+        //return $var;
+        return "hola";
+    }
+
+}
+
